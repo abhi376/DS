30a06ef0360e2ab81e719277e12b646a0667243e
Abhishek Verma <abhishek.verma@ivycomptech.com>
2020-04-11 12:38:09 +0530

    added description for min stack question

--- CPP/DS/Stack/MinStack.cpp
+++ CPP/DS/Stack/MinStack.cpp
@@ -5,7 +5,28 @@
 //  Created by Abhishek Verma on 4/11/20.
 //  Copyright © 2020 Abhishek Verma. All rights reserved.
 //
+/*
+ Solution
+ Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
 
+ push(x) -- Push element x onto stack.
+ pop() -- Removes the element on top of the stack.
+ top() -- Get the top element.
+ getMin() -- Retrieve the minimum element in the stack.
+  
+
+ Example:
+
+ MinStack minStack = new MinStack();
+ minStack.push(-2);
+ minStack.push(0);
+ minStack.push(-3);
+ minStack.getMin();   --> Returns -3.
+ minStack.pop();
+ minStack.top();      --> Returns 0.
+ minStack.getMin();   --> Returns -2.
+ https://leetcode.com/explore/featured/card/30-day-leetcoding-challenge/529/week-2/3292/
+ */
 #include <stdio.h>
 #include <stack>
 
